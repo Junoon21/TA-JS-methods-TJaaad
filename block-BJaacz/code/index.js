828b9ecfdadb25c7563b2ce8916bff2ba1025780
Junoon21 <ritikasingh9006@gmail.com>
2022-01-13 14:39:14 +0530

    1st commit

--- block-BJaacz/code/index.js
+++ block-BJaacz/code/index.js
@@ -3,36 +3,74 @@
 /*
   Create a function named addTwo which accepts a number, adds 2 to the number and return the new value.
 */
-
+function addTwo(num) {
+ return num + 2 ;
+}
 /*
   Create a function named addThree which accepts a number, adds 3 to the number and return the new value.
 */
-
+function addThree(num) {
+  return num + 3 ;
+}
 /*
   Create a function named addFive which accepts a number, adds 5 to the number and return the new value.
 */
-
+function addFive(num) {
+  return num + 5 ;
+}
 /*
   Create a function named addTwoToArray which accepts:
     - An array of numbers
     - Returns a new array where 2 is added to each element
     - While doing so use the funciton addTwo
 */
+let numbers = [1,2,3,4,5,6,7,8];
 
+function addTwoToArray (arr) {
+  let newArray = [] ;
+  for(let number of arr){
+    if(addTwo(number)){
+      newArray.push = number;
+    }
+  }
+  return newArray;
+}
+let finalArray=(addTwoToArray(numbers));
+console.log(finalArray);
 /*
   Create a function named addThreeToArray which accepts:
     - an array of numbers
     - aeturns a new array where 2 is added to each element
     - while doing so use the funciton addThree
 */
-
+function addThreeToArray (arr) {
+  let newArray = [] ;
+  for(let number of arr){
+    addThree(number);
+    newArray.push = number;
+  }
+  return newArray;
+}
+let finalArray=(addThreeToArray(numbers));
+console.log(finalArray);
 /*
   Create a function named addFiveToArray which accepts:
     - an array of numbers
     - aeturns a new array where 2 is added to each element
     - while doing so use the funciton addThree
 */
-
+function addFiveToArray (arr) {
+  let newArray = [] ;
+  for(let number of arr){
+    if(addTwo(number)){
+      newArray.push = number;
+    }
+ 
+  }
+  return newArray;
+}
+let finalArray=(addFiveToArray(numbers));
+console.log(finalArray);
 /*
 In above function addTwoToArray, addThreeToArray, addFiveToArray we are repeating the code, let's fix this.
 
